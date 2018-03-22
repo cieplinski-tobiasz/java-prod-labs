fd2c8a1a62497162a120bc822a7716376d938d87
Tobiasz Ciepliński <cieplinski.t@gmail.com>
2018-03-22 11:58:15 +0100

    lab03ex1 shadowjar fix

--- lab-3/ex-1/src/main/java/app/Application.java
+++ lab-3/ex-1/src/main/java/app/Application.java
@@ -2,15 +2,44 @@ package app;
 
 import com.external.PaymentsService;
 import com.internal.DiscountCalculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
 
 public class Application {
-    public static void main(String[] args) {
-        DiscountCalculator discountCalculator = new DiscountCalculator();
-        PaymentsService paymentsService = new PaymentsService();
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
 
-        discountCalculator.calculateDiscount(BigDecimal.ONE, 1);
-        paymentsService.makePayment(1, 2, BigDecimal.ONE);
+    private DiscountCalculator discountCalculator = new DiscountCalculator();
+    private PaymentsService paymentsService = new PaymentsService();
+
+    public void run(BigDecimal ticketPrice, int customerAge, int customerId, int companyId) {
+        BigDecimal amount = discountCalculator.calculateDiscount(ticketPrice, customerAge);
+        paymentsService.makePayment(customerId, companyId, amount);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 4) {
+            logger.error("Bad arguments");
+            throw new IllegalStateException();
+        }
+
+        BigDecimal ticketPrice = null;
+        int customerAge = 0;
+        int customerId = 0;
+        int companyId = 0;
+
+        try {
+            ticketPrice = new BigDecimal(args[0]);
+            customerAge = Integer.valueOf(args[1]);
+            customerId = Integer.valueOf(args[2]);
+            companyId = Integer.valueOf(args[3]);
+        } catch (NumberFormatException e) {
+            logger.error("Could not parse arguments", e);
+            System.out.println("Błędne argumenty.");
+        }
+
+        Application application = new Application();
+        application.run(ticketPrice, customerAge, customerId, companyId);
     }
 }
